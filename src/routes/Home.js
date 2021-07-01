4e9bd88bcf2a1879ed38d6f4b02aaa1647ce50d7
sangjun <sgbo5003@naver.com>
2021-07-01 18:08:34 +0900

    #3.0 Form and Database Setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
-
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          onChange={onChange}
+          placeholder="what's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Ntweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
